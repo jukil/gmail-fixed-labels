ff8214de549bcd44af6e40817e6ffca9e2a70f28
Robert Orzanna <orschiro@gmail.com>
2013-10-05 15:14:24 +0200

    Added position code from gmail-righttasks

--- default.js
+++ default.js
@@ -12,3 +12,30 @@ function hashCheck() {
 }
 window.addEventListener('hashchange', hashCheck);
 hashCheck();
+
+var position = function() {
+
+        currentTopPosition = $mailContainer.offsetTop;
+
+        /* In case we the usual container isn't there
+         */
+        if(currentTopPosition === 0) {
+            var $gbox1 = document.getElementById('gbx1');
+
+            /* Compatibility with Streak for Gmail, Chrome extension
+             */
+            var streakContainer = document.getElementsByClassName('pv_main_container')[0];
+            if(streakContainer) {
+                currentTopPosition = streakContainer.offsetTop;
+            } else {
+                currentTopPosition = $gbox1.offsetTop + $gbox1.clientHeight;
+            }
+        }
+
+        // set the widget top to match the main container top
+        if($tasksContainer && currentTopPosition!== lastTopPosition) {
+            $tasksContainer.style.paddingTop = currentTopPosition + 'px';
+            lastTopPosition = currentTopPosition;
+        }
+
+    };
